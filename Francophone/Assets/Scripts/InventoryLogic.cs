595391364e914be132378d74e33980464a7eb4e2
Greg <bowinsg@sheridancollege.ca>
2018-10-12 13:00:12 -0400

    Inventory patch

---
+++ Francophone/Assets/Scripts/InventoryLogic.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
+using System;
+
+public class InventoryLogic : MonoBehaviour {
+
+    List<Item> PlayerInventory = new List<Item>();
+    List<Item> ItemList;
+    public Transform InventoryContent;
+    public SimpleObjectPool ItemBtnPool;
+    public GameObject InventoryPanel;
+    public GameObject MovementUI;
+    public GameObject btnPrefab;
+    public GameObject btnInventory;
+
+    // Use this for initialization
+    private void Start()
+    {
+        InventoryPanel.gameObject.SetActive(false);
+
+        //for future when save file is enabled
+        if (ItemList == null)
+        {
+            InitItemList();
+        }
+    }
+
+    private void InitItemList()
+    {
+        
+        XmlSerializer deserializer = new XmlSerializer(typeof(List<Item>), new XmlRootAttribute("Items"));
+        TextReader textReader = new StreamReader(@"Assets/Data/ItemList.xml");
+        ItemList = (List<Item>)deserializer.Deserialize(textReader);
+        textReader.Close();
+        ItemList.Sort((x, y) => x.ItemName.CompareTo(y.ItemName));
+        /*  *
+        AddItem("Word");
+        AddItem("Random");
+        AddItem("Random");
+        /**/
+        /*  To show dynamic list scrolling */
+        for(int i = 0; i <= 100; i++)
+        {
+            AddItem("Word");
+        }
+        /**/
+    }
+
+    [YarnCommand("AddItem")]
+    public void AddItem(string itemName)
+    {
+        /* To show dynamic list scrolling */
+        Item newItem = ItemList.Find(item => item.ItemName == itemName);
+        PlayerInventory.Add(newItem);
+        SortInv();
+        /**/
+        /* *
+        bool existingItem = PlayerInventory.Any(item => item.ItemName == itemName);
+        if (existingItem)
+        {
+            int itemIndex = PlayerInventory.FindIndex(item => item.ItemName == itemName);
+            PlayerInventory[itemIndex].Quantity++;
+        }
+        else
+        {
+            Item newItem = ItemList.Find(item => item.ItemName == itemName);
+            PlayerInventory.Add(newItem);
+            SortInv();
+        }
+        /**/
+    }
+
+    public void RemoveItem(string itemName)
+    {
+        bool existingItem = PlayerInventory.Any(item => item.ItemName == itemName);
+        if (existingItem)
+        {
+            int itemIndex = PlayerInventory.FindIndex(item => item.ItemName == itemName);
+            PlayerInventory.RemoveAt(itemIndex);
+            SortInv();
+        }
+        RefreshDisplay();
+    }
+
+    public void SortInv()
+    {
+        PlayerInventory.Sort((x, y) => x.ItemName.CompareTo(y.ItemName));
+    }
+
+    public void OpenInventory()
+    {
+        MovementUI.SetActive(false);
+        //RemoveButtons();
+        //RefreshDisplay();
+        foreach (Item item in PlayerInventory)
+        {
+            if (item.MarkedForDelete)
+            {
+                RemoveItem(item.ItemName);
+                continue;
+            }
+            GameObject newItem = ItemBtnPool.GetObject();
+            newItem.transform.SetParent(InventoryContent);
+            InventoryLabels inventoryLabel = newItem.GetComponent<InventoryLabels>();
+            inventoryLabel.Setup(item);
+            newItem.GetComponent<RectTransform>().localScale = btnPrefab.GetComponent<RectTransform>().localScale;
+        }
+        InventoryPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseInventory()
+    {
+        MovementUI.SetActive(true);
+        InventoryPanel.gameObject.SetActive(false);
+    }
+
+    [YarnCommand("EquipBackpack")]
+    public void EquipFrancophone()
+    {
+        btnInventory.SetActive(true);
+    }
+
+    private void RemoveButtons()
+    {
+        //Infinite loop, need to fix
+        while (InventoryContent.childCount > 0)
+        {
+            GameObject toRemove = InventoryContent.GetChild(0).gameObject;
+            ItemBtnPool.ReturnObject(toRemove);
+        }
+    }
+
+    public void RefreshDisplay()
+    {
+        //RemoveButtons();
+        foreach (Item item in PlayerInventory)
+        {
+            if (item.MarkedForDelete)
+            {
+                RemoveItem(item.ItemName);
+                continue;
+            }
+            GameObject newItem = ItemBtnPool.GetObject();
+            newItem.transform.SetParent(InventoryContent);
+            InventoryLabels inventoryLabel = newItem.GetComponent<InventoryLabels>();
+            inventoryLabel.Setup(item);
+        }
+    } 
+}
---
+++ Francophone/Assets/Scripts/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item {
+
+    DictionaryLogic dictionaryLogic;
+
+    public string ItemName { get; set; }
+
+    public int Quantity { get; set; }
+
+    public int ImageNumber { get; set; }
+
+    public bool MarkedForDelete { get; set; }
+    //public GameObject ItemLogic;
+    public Item()
+    {
+    }
+    
+    public void WordPotion()
+    {
+        dictionaryLogic = GameObject.Find("GENERAL").GetComponent<DictionaryLogic>();
+        dictionaryLogic.AddWord("Rouge");
+    }
+
+    public void RandomPotion()
+    {
+        
+    }
+}
