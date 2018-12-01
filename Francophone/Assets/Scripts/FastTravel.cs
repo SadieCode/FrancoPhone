6a3091eab54ad85c40deeb70622d69fc422e6a21
Mercedez Shuttleworth <shuttlem@sheridancollege.ca>
2018-11-30 20:37:04 -0500

    Added Daniel's fast travel script, removed old movement script

---
+++ Francophone/Assets/Scripts/FastTravel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastTravel : MonoBehaviour {
+
+    public GameObject panel;
+    //public GameObject panelSchool;
+
+    public GameObject player;
+    private Vector3 fastTravelPosition;
+    public Transform target;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerLogic>() != null)
+        {
+            collision.gameObject.transform.position = target.position;
+            Camera.main.transform.position = fastTravelPosition;
+        }
+       
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        panel.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+        panel.SetActive(false);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
